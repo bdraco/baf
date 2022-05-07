3f8385b4e9da5365d80b0665303b2a2c021ed62e
J. Nick Koston <nick@koston.org>
2022-05-07 11:57:32 -0500

    add sensor platform

--- __init__.py
+++ __init__.py
@@ -18,6 +18,7 @@ PLATFORMS: list[Platform] = [
     Platform.CLIMATE,
     Platform.FAN,
     Platform.LIGHT,
+    Platform.SENSOR,
     Platform.SWITCH,
 ]
 
@@ -37,6 +38,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     except asyncio.TimeoutError as ex:
         run_task.cancel()
         raise ConfigEntryNotReady(f"Timed out connecting to {ip_address}") from ex
+
+    # Temporary workaround until the upstream lib is fixed
+    # to ensure we get the name
+    for _ in range(50):
+        if not device.name:
+            await asyncio.sleep(0.1)
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = BAFData(device, run_task)
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
